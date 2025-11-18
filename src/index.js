80ce1aa214fc49bacfeb16aaabbaf25dfa385cc5
muhammad-25 <vaazi17@gmail.com>
2025-11-18 19:56:03 +0700

    tambah navbar

--- src/index.js
+++ src/index.js
@@ -18,7 +18,9 @@ app.use(expressLayouts);
 app.set('layout', 'layouts/main'); // layout default (views/layouts/main.ejs)
 
 // static files (css/js/images)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+
 
 
 app.get('/', (req, res) => {
