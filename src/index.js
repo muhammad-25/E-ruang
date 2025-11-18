3d46030837cd2287d70480a789e3c299a2feb6dc
muhammad-25 <vaazi17@gmail.com>
2025-11-18 17:52:24 +0700

    setiings bagian views

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 const app = express()
 const port = 3000
 const bodyParser = require("body-parser")
@@ -7,41 +9,23 @@ const  db = require("../database")
 // ubah info yang di terima ke bentuk json
 app.use(bodyParser.json())
 
-console.log ("tess")
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..', 'views'));
+
+
+// pakai express-ejs-layouts
+app.use(expressLayouts);
+app.set('layout', 'layouts/main'); // layout default (views/layouts/main.ejs)
+
+// static files (css/js/images)
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
-  // ngambil data atau query yang ada di url, dengan key value username (cek postman : params)
-  const nama = req.query.username
-  res.send(('Hello ' + nama), )
-})
-
-app.get('/login',(req,res) => {
-  db.query('SELECT * FROM users', (err, results) => {
-    if (err) {
-      console.error('Error saat ambil data:', err);
-      return res.status(500).send('Gagal ambil data');
-    } else {
-      console.log(results); 
-    }
-  });
-  res.send('ini halaman login')
-})  
-
-app.get('/username',(req,res) => {
-  res.send('ini halaman username')
-})
+  res.render('pages/index', { title: 'Beranda', user: 'Vaazi' });
+});
 
 
-// post itu bisa minta rquest dan ngirim respon tanpa terlihat oleh user
-app.post('/login', (req,res) => {
-  // minta request dari body
-  console.log({"req dari luar" : req.body})  
-  res.send("login berhasil")
-})
 
-app.put("/username", (req,res) => {
-  console.log({ "updateData" : req.body })
-  res.send("update berhasil!")
-})
 
 module.exports = app;
