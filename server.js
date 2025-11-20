81397f19c6b5ec9e956f6c09bdbafd73cd3f655c
muhammad-25 <vaazi17@gmail.com>
2025-11-20 23:36:55 +0700

    menambahkan halaman index

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const port = 3000
 
-const app = require("./src/index.js")
+const app = require("./src/router.js")
 app.listen(port, () => {
   console.log(`E-ruang listening on port ${port}`)
 })
